d3a56800c5af71cd43cf6f6e93a060e18002dd75
Salvador <salvahg03@gmail.com>
2022-03-09 19:56:06 -0600

    Actividades JS Dìa 1

---
+++ IntroJS/Clase1/Actividad1.js
@@ -0,0 +1,30 @@
+// Ejercicio 1 -> Ingresar un nùmero y multiplicar por dos.
+
+var number = Number(prompt("Ingresa un nùmero aleatorio"));
+var result = number * 2;
+console.log(result);
+
+
+// Ejercicio 2 -> Pedir dos nùmeros y restarlos entre si
+
+var number1 = Number(prompt("Ingresa un nùmero"));
+var number2 = Number(prompt("Ingresa otro nùmero para restar"));
+var result = number1 - number2;
+console.log(result);
+
+
+// Ejercicio 3 -> Calcular aea de un circulo con radio de 5.3
+
+var pi = 3.1416;
+var radius = 5.3;
+var result = pi * radius ** 2;
+console.log(result);
+
+
+// Calcular àrea de un rombo con los siguientes datos:
+
+var p = 5;
+var D = 8;
+var d = 6;
+var result = (D * d) / 2;
+console.log(result)
